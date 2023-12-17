4008fdc04bd611cda6da6e42bee0c82a89bc8570
utkarsh <utkarshsinghrajput16@gmail.com>
2023-12-17 15:45:52 +0530

    feat : main_

--- app/src/main/java/com/orion/templete/MyApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.orion.templete
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MyApplication: Application(){
-
-}
--- app/src/main/java/com/orion/templete/domain/use_case/Screens.kt
+++
@@ -1,5 +0,0 @@
-package com.orion.templete.domain.use_case
-
-sealed class Screen(val route: String){
-    object MainScreen : Screen("main_screen")
-}
